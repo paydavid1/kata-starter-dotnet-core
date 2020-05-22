974d5c61de38f2385ab04d4c67a49cfd37920831
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-05-22 14:04:58 -0600

    User input empty failing

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -19,6 +19,17 @@ namespace Kata.Spec
     //test github repo
     //test local pull
     // Given the user input is empty when calculating the sum then it should return zero.
+    public class when_user_input_is_empty
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add(); };
+
+        It should_return_zero = () => { _result.Should().Be(0); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+
     // Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
     // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
     // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
